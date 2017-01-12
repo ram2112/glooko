2391ec808975fc4bdcd27e4d6423943718af35f3
Ramakrishnan Pitchai Kumar <ram90.pk@gmail.com>
2017-01-11 17:01:26 -0800

    Delete keys.r

--- keys.r
+++
@@ -1,8 +0,0 @@
-# keys.R
-
-
-id <- "qJqRdou2AMHMzIhaTEFDkiIJ6"
-pw <- "Ap6YewfTr5g3dBCnP3EAHG8d3Wj8jo481i4bzwhPcKUaDISYM5"
-token <- "785237825486589952-2l8ZQYBdAYP99L1g9KqSsj3DJahuua0"
-secret <- "gAYIiDv3qYjKCQup4RVWF5jG04rNlqJXWNmnvmZbnK9wI"
-klout_key <- 'pxusnuwjcsj9zsakdr2xqn9g'
